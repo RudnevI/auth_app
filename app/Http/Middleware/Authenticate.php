0dc3d000444146ac54b12e185165104aac9f2162
RudnevI <irudnev@protonmail.com>
2022-02-21 19:35:37 +0600

    add some services

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Service\TokenService;
 use Closure;
 use Exception;
 
@@ -16,33 +17,6 @@ class Authenticate
      * @return mixed
      */
     public function handle($request, Closure $next) {
-        $bearerToken = $request->bearerToken();
-
-        if(empty($bearerToken)) {
-
-            return response()->json(["Message"=>"Request is unauthorized: token is empty"], 403);
-        }
-        $explodedToken = [];
-        try {
-            $explodedToken = explode(".", $bearerToken);
-            if(count($explodedToken) < 3) {
-                throw new Exception();
-            }
-        }
-        catch(Exception $e) {
-
-            return response()->json(["Message"=>"Unsupported token format"], 400);
-        }
-        $signature = $explodedToken[2];
-        $header = $explodedToken[0];
-        $payload = $explodedToken[1];
-
-
-
-        if(!strcmp($signature, hash_hmac("sha256", $header .'.'. $payload, env("JWT_SECRET")))===0) {
-            return response()->json(["Message"=>"Token is not valid"], 400);
-        }
-        return $next($request);
-
+            TokenService::validateToken($request, $next);
     }
 }
--- app/Http/Service/TokenService.php
+++ app/Http/Service/TokenService.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Service;
 use Carbon\Carbon;
+use Closure;
+use Exception;
 
 class TokenService {
 
@@ -21,5 +23,36 @@ class TokenService {
 
     public static function getExpirationDate() {
         return Carbon::createFromTimestampMs(env("TOKEN_EXPIRATION_TIME") + Carbon::now()->valueOf());
+    }
+
+
+    public static function validateToken($request, Closure $next) {
+        $bearerToken = $request->bearerToken();
+
+        if(empty($bearerToken)) {
+
+            return response()->json(["Message"=>"Request is unauthorized: token is empty"], 403);
+        }
+        $explodedToken = [];
+        try {
+            $explodedToken = explode(".", $bearerToken);
+            if(count($explodedToken) < 3) {
+                throw new Exception();
+            }
+        }
+        catch(Exception $e) {
+
+            return response()->json(["Message"=>"Unsupported token format"], 400);
+        }
+        $signature = $explodedToken[2];
+        $header = $explodedToken[0];
+        $payload = $explodedToken[1];
+
+
+
+        if(!strcmp($signature, hash_hmac("sha256", $header .'.'. $payload, env("JWT_SECRET")))===0) {
+            return response()->json(["Message"=>"Token is not valid"], 400);
+        }
+        return $next($request);
     }
 }
